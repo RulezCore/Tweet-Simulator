f52549839847ad628e291206476fc897170596d2
Daniel Casado <ixrulez69@gmail.com>
2020-06-22 02:24:27 +0200

    localStorage

--- src/components/FormSendTweet/FormSendTweet.js
+++ src/components/FormSendTweet/FormSendTweet.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { FormControl, FormGroup, TextField, Button } from "@material-ui/core";
 import "./FormSendTweet.scss";
 
 const FormSendTweet = (props) => {
-  const {} = props;
+  const { sendTweet } = props;
+
+  const [formValue, setFormValue] = useState({
+    name: "",
+    tweet: "",
+  });
+
+  const onFormChange = (event) => {
+    setFormValue({
+      ...formValue,
+      [event.target.name]: event.target.value,
+    });
+  };
 
   return (
     <div className="form-send-tweet">
       <h2 className="form-send-tweet__title">Enviar Tweet!</h2>
-      <form onSubmit="" className="form-send-tweet__form">
+      <form
+        className="form-send-tweet__form"
+        onSubmit={(event) => sendTweet(event, formValue)}
+        onChange={onFormChange}
+      >
         <FormControl>
           <FormGroup>
             <TextField
--- src/components/SendTweet/SendTweet.js
+++ src/components/SendTweet/SendTweet.js
@@ -3,6 +3,7 @@ import { Fab } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 import ModalContainer from "../ModalContainer";
 import FormSendTweet from "../FormSendTweet";
+import { TWEETS_STORAGE } from "../../utils/constants";
 import moment from "moment";
 
 // CSS
@@ -19,6 +20,26 @@ const SendTweet = () => {
     setIsOpenModal(false);
   };
 
+  const sendTweet = (event, formValue) => {
+    event.preventDefault();
+
+    // Guardar en localStorage
+    const { name, tweet } = formValue;
+    let allTweetsArray = [];
+
+    if (!name || !tweet) {
+      console.log("Todos los campos son obligatorios!");
+    } else {
+      formValue.time = moment();
+      allTweetsArray.push(formValue);
+      localStorage.setItem(TWEETS_STORAGE, JSON.stringify(allTweetsArray));
+      console.log("Tweet enviado y almacenado!");
+      closeModal();
+    }
+
+    allTweetsArray = [];
+  };
+
   return (
     <div className="send-tweet">
       <Fab
@@ -30,7 +51,7 @@ const SendTweet = () => {
         <AddIcon />
       </Fab>
       <ModalContainer isOpenModal={isOpenModal} closeModal={closeModal}>
-        <FormSendTweet />
+        <FormSendTweet sendTweet={sendTweet} />
       </ModalContainer>
     </div>
   );
